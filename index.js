e613a310bddb688d6c8b6cff01039c22efcbbdf0
Lauren Schwartz <lschwartzwriting@gmail.com>
2017-12-08 18:39:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ function dec(n){
   return (n -= 1)
 }
 function makeInt(n){
-  parseInt('n', 10)
+  parseInt('n')
   return (n)
 }
 function preserveDecimal(n){
-  parseFloat("n")
+  parseFloat('n')
   return (n)
 }
